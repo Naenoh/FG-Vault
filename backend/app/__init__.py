e845cb0c4f7fdfbff12f82f7326f73d7d681611e
DIDES ARNAUD p1610215 <arnaud.dides@etu.univ-lyon1.fr>
2017-12-26 21:41:05 +0100

    added TODO and removed database file

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fgctechuser:fgctechpass10@127.0.0.1:5432/fgctech'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fgctechuser:fgctechpass10@127.0.0.1:5432/fgctech'  # TODO hide this
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 

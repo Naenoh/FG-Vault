fc5daeb71c4af5236a3d67d7eae5aab0a124e75b
DIDES ARNAUD p1610215 <arnaud.dides@etu.univ-lyon1.fr>
2017-12-28 16:00:38 +0100

    Updated models (chars and links)

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,18 +2,20 @@
 # from sqlalchemy.orm import relationship
 # from .database import Base
 from app import db
+
 Column = db.Column
 Integer = db.Integer
 String = db.String
 relationship = db.relationship
 ForeignKey = db.ForeignKey
 
+
 class Post(db.Model):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
     title = Column(String(255))
     game_id = Column(Integer, ForeignKey('games.id'))
-    game = relationship('Game', backref='post')
+    game = relationship('Game', backref='posts')
 
     def __repr__(self):
         return '<Post %r>' % self.title
@@ -27,3 +29,24 @@ class Game(db.Model):
     def __repr__(self):
         return '<Game %r>' % self.name
 
+
+class Char(db.Model):
+    __tablename__ = 'chars'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), unique=True)
+    game_id = Column(Integer, ForeignKey('games.id'))
+    game = relationship('Game', backref='chars')
+
+    def __repr__(self):
+        return '<Char %r>' % self.name
+
+
+class Link(db.Model):
+    __tablename__ = 'links'
+    id = Column(Integer, primary_key=True)
+    url = Column(String(255), unique=True)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    post = relationship('Post', backref='links')
+
+    def __repr__(self):
+        return '<Link %r>' % self.url

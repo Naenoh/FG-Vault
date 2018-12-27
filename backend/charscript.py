db94db91e90989e369e1cfe956a830420ac9b993
DIDES ARNAUD p1610215 <arnaud.dides@etu.univ-lyon1.fr>
2018-12-27 14:17:54 +0100

    Updated sql generation script to use arguments for input and output

--- backend/charscript.py
+++ backend/charscript.py
@@ -1,17 +1,23 @@
-with open("gamechars.txt", encoding="utf-8") as infile:
-    with open("gamechars.sql", "w", encoding="utf-8") as outfile:
-        outfile.write("BEGIN;")
-        game = ""
-        for line in infile:
-            line = line.strip()
-            if line.startswith("#"):
-                if(game != ""):
-                    outfile.write(";\n\n")
-                game = line[1:]
-                outfile.write("INSERT INTO games (name) VALUES ('{}');\n".format(game))
-                outfile.write("INSERT INTO chars (name, game_id) VALUES")
-                outfile.write("('General', (SELECT id from games where name = '{}'))\n".format(game))
-            else:
-                outfile.write(",('{}', (SELECT id from games where name = '{}'))\n".format(line,game))
-        outfile.write(";\n")
-        outfile.write("COMMIT;")
+import sys
+
+if len(sys.argv) != 3:
+    print("Too few or too many arguments")
+    print("Usage : python charscript.py inputfile outputfile")
+else:
+    with open(sys.argv[1], encoding="utf-8") as infile:
+        with open(sys.argv[2], "w", encoding="utf-8") as outfile:
+            outfile.write("BEGIN;")
+            game = ""
+            for line in infile:
+                line = line.strip()
+                if line.startswith("#"):
+                    if(game != ""):
+                        outfile.write(";\n\n")
+                    game = line[1:]
+                    outfile.write("INSERT INTO games (name) VALUES ('{}');\n".format(game))
+                    outfile.write("INSERT INTO chars (name, game_id) VALUES")
+                    outfile.write("('General', (SELECT id from games where name = '{}'))\n".format(game))
+                else:
+                    outfile.write(",('{}', (SELECT id from games where name = '{}'))\n".format(line,game))
+            outfile.write(";\n")
+            outfile.write("COMMIT;")
